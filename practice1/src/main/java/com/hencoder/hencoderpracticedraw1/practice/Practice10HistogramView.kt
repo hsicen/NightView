65b3637f7b3d6d8da5f530321fd4889ca7bbcb8d
night <codinghuang@163.com>
2019-01-10 23:26:38 +0800

    绘制简易直方图和Path路径绘制几何图形

--- practice1/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.kt
+++ practice1/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.kt
@@ -1,11 +1,14 @@
 package com.hencoder.hencoderpracticedraw1.practice
 
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
 class Practice10HistogramView : View {
+    var mPaint = Paint()
+    var mPath = Path()
+    var mTextRect = Rect()
 
     constructor(context: Context) : super(context) {}
 
@@ -18,5 +21,44 @@ class Practice10HistogramView : View {
 
         //        综合练习
         //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+
+        //绘制坐标系
+        mPaint.style = Paint.Style.STROKE
+        mPaint.isAntiAlias = true
+        mPaint.strokeWidth = 4f
+        mPaint.color = Color.WHITE
+        mPath.moveTo(100f, 50f)
+        mPath.lineTo(100f, 600f)
+        mPath.lineTo(1000f, 600f)
+        canvas.drawPath(mPath, mPaint)
+
+        //绘制字
+        val mText = "直方图"
+        mPaint.textSize = 60f
+        mPaint.style = Paint.Style.FILL
+        mPaint.strokeWidth = 1f
+        mPaint.getTextBounds(mText, 0, mText.length, mTextRect)
+        canvas.drawText(mText, 500f - mTextRect.width() / 2, 750f, mPaint)
+
+        //绘制方块  900÷7=128
+        mPaint.color = Color.GREEN
+        canvas.drawRect(120f, 595f, 220f, 600f, mPaint)
+        canvas.drawRect(240f, 580f, 340f, 600f, mPaint)
+        canvas.drawRect(360f, 580f, 460f, 600f, mPaint)
+        canvas.drawRect(480f, 400f, 580f, 600f, mPaint)
+        canvas.drawRect(600f, 300f, 700f, 600f, mPaint)
+        canvas.drawRect(720f, 200f, 820f, 600f, mPaint)
+        canvas.drawRect(840f, 400f, 940f, 600f, mPaint)
+
+        //绘制字
+        mPaint.color = Color.WHITE
+        mPaint.textSize = 30f
+        canvas.drawText("L", 760f, 630f, mPaint)
+        canvas.drawText("KitKat", 610f, 630f, mPaint)
+        canvas.drawText("JB", 510f, 630f, mPaint)
+        canvas.drawText("ICS", 390f, 630f, mPaint)
+        canvas.drawText("GB", 270f, 630f, mPaint)
+        canvas.drawText("M", 880f, 630f, mPaint)
+        canvas.drawText("Froyo", 140f, 630f, mPaint)
     }
 }
--- practice1/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.kt
+++ practice1/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.kt
@@ -2,10 +2,22 @@ package com.hencoder.hencoderpracticedraw1.practice
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
 class Practice9DrawPathView : View {
+    var mPaint = Paint()
+    var mPath = Path()
+
+    init {
+        mPath.addArc(200f, 200f, 400f, 400f, -225f, 225f)
+        mPath.arcTo(400f, 200f, 600f, 400f, -180f, 225f, false)
+        mPath.lineTo(400f, 542f)
+
+        mPath.addCircle(600f, 600f, 100f, Path.Direction.CW)
+    }
 
     constructor(context: Context) : super(context) {}
 
@@ -17,6 +29,6 @@ class Practice9DrawPathView : View {
         super.onDraw(canvas)
 
         //        练习内容：使用 canvas.drawPath() 方法画心形
-
+        canvas.drawPath(mPath, mPaint)
     }
 }
